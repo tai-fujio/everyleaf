8a938943d9f36b89427d19b5620b7aaae77cd0e0
tai-fujio <taisaburo.fujio@gmail.com>
2020-06-17 01:16:49 +0900

    mirror the e original

---
+++ app/assets/javascripts/tasks.js
@@ -0,0 +1,12 @@
+// 『8-2-1: Ajax でタスクを削除する』で記述するコード.
+// 『8-2-3: コントローラからJavaScriptを返して実行する』で不要になるためにコメントアウトする.
+//
+// document.addEventListener('turbolinks:load', function() {
+//   document.querySelectorAll('.delete').forEach(function(a) {
+//     a.addEventListener('ajax:success', function() {
+//       var td = a.parentNode;
+//       var tr = td.parentNode;
+//       tr.style.display = 'none';
+//     });
+//   });
+// });
